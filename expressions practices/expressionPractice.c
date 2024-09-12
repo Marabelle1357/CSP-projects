5a06172b1164353a6442a46e859cc97a1e0b18ff
Marabelle1357 <mara.anderson@ucas-edu.net>
2024-09-12 03:59:17 +0000

    deleted an unwanted file and continued coding on expression practice for c

--- expressions practices/expressionPractice.c
+++ expressions practices/expressionPractice.c
@@ -3,14 +3,39 @@
 
 int main(void){
     int frstEquation = (int) (7-24/8*4+6);
-    printf("%d", frstEquation );
-
-
-
-
-
-
-
     
-    return 0;
+    int scndEquation = (int) (18/3-7+2*5);
+
+    int thrdEquation = (int) (6*4/12+72/8-9);
+
+    int frthEquation = (int) (17-6/2+4*3);
+
+    int fthEquation = (int) (-2*1*-2*4+2*-2/-2*2+6+2-3);
+    
+    int sxthEquation = (int) ((-1)*((3-(4*7))/5))-((2*24)/6);
+
+    int svnthEquation = (int) ((3*(5*5))/15)-(5-(2*2));
+
+    int egthEquation = (int) (((1*1*1*1)*(2*2))+(3*3))-((2*2*2*2*2)/4);
+
+    int nthEquation = (int) (1-1);
+
+    printf("%d", frstEquation);
+
+    printf("%d", scndEquation);
+
+    printf("%d", thrdEquation);
+
+    printf("%d", frthEquation);
+
+    printf("%d", fthEquation);
+
+    printf("%d", sxthEquation);
+
+    printf("%d", svnthEquation);
+
+    printf("%d", egthEquation);
+
+    printf("%d", nthEquation);
+        return 0;
 }
