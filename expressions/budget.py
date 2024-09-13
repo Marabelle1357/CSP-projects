ef4e57234d70023b8386313f45f52a57a542ce7e
Marabelle1357 <mara.anderson@ucas-edu.net>
2024-09-13 05:20:04 +0000

    finished editing budget for python

--- expressions/budget.py
+++ expressions/budget.py
@@ -1,22 +1,29 @@
 print("This code will calculate your bugdet for the month.")
 income = input("How much do you make in a month?\n")
+Iincome = int(income)
 rent = input("How much do you pay rent in a month?\n")
+Irent = int(rent)
 utilities = input("How much do you pay in utilities in a month?\n")
+Iutil = int(utilities)
 groceries = input("How much do you pay in groceries in a month?\n")
+Igroc = int(groceries)
 transportation = input("How much do you pay in transportation in a month?\n")
-expenses = rent + utilities + groceries + transportation
-savings = income + 1*0.2/1
-total = income-expenses-savings
-prent = rent/income
-putilities = utilities/income
-pgroceries = groceries/income
-ptranspotation = transportation/income
-pexpenses = expenses/income
-print("Your income is: $%.2f\n", income)
-print("Your expenses are: $%.2f\n", expenses)
-print("Your savings are: $%.2f\n", savings)
-print("Your total left to spend is: $%.2f\n", total)
-print("Your rent payment is: %.2f", prent, "of your expenses")
-print("Your utilities payment is: %.2f", putilities, "of your expenses")
-print("Your groceries payment is: %.2f", pgroceries, "of your expenses")
-print("Your transportation payment is: %.2f", ptranspotation, "of your expenses")
+Itrans = int(transportation)
+expenses = Irent + Iutil + Igroc + Itrans
+Iexp = int(expenses)
+savings = Iincome *0.2
+Fsav = float(savings)
+total = Iincome-Iexp+Fsav
+prent = Irent/Iincome
+putilities = Iutil/Iincome
+pgroceries = Igroc/Iincome
+ptranspotation = Itrans/Iincome
+pexpenses = Iexp/Iincome
+print("Your income is: $", income)
+print("Your expenses are: $", expenses)
+print("Your savings are: $", savings)
+print("Your total left to spend is: $", total)
+print("Your rent payment is: ", prent, "percent of your expenses")
+print("Your utilities payment is: ", putilities, " percent of your expenses")
+print("Your groceries payment is: ", pgroceries, " percent of your expenses")
+print("Your transportation payment is: ", ptranspotation, " percent of your expenses")
