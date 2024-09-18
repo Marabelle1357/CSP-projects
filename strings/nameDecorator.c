0996e26931ce86cbc3790fe77ca550dd76b05654
Marabelle1357 <mara.anderson@ucas-edu.net>
2024-09-18 20:41:42 +0000

    finished coding c assignment

--- strings/nameDecorator.c
+++ strings/nameDecorator.c
@@ -4,10 +4,10 @@ int main(void){
     char name[15];
     printf("What is your name?: \n");
     scanf("%s", name);
-    char beg = ("###");
-    char end = ("###");
+    char beg[10] = ("###");
+    char end[10] = ("###");
     strcat(beg,name);
     strcat(beg,end);
-    printf("%s",beg);
+    printf("%s", beg);
     return 0;
 }
