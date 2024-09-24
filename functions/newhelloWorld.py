f5b1e554442b2cebffb16db735799503c8e9207e
Marabelle1357 <mara.anderson@ucas-edu.net>
2024-09-24 20:24:17 +0000

    started and finished coding python project

---
+++ functions/newhelloWorld.py
@@ -0,0 +1,9 @@
+def names(input):
+    
+    return f"Hello{input}"
+
+print(names(" Starli(oldest parent)"))
+print(names(" Mike(youngest parent)"))
+print(names(" Alex(oldest child)"))
+print(names(" Mara(middle child)"))
+print(names(" Madi(youngest child)"))
