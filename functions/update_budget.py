82eb8f4d6faaaf52f72dff54e8935395ae87631d
Marabelle1357 <mara.anderson@ucas-edu.net>
2024-09-27 20:41:28 +0000

    coded project

--- functions/update_budget.py
+++ functions/update_budget.py
@@ -1,15 +1,11 @@
-
 def inp(type):
-    input("How much is your "+type+"?\n")
-    return f"How much is your "+type+"?\n"
+    var = float(input("What is your{type}?"))
+    return var
 
-def percent(type, amount):
-    per = amount/income*100
-    print("Your"+type+"is"+per+"% of your income.\n")
 
 print("This code will calculate your bugdet for the month.")
-income = inp("income")
-rent = inp("rent")
+inp("income")
+inp("rent")
 utilities = input("How much do you pay in utilities in a month?\n")
 Iutil = int(utilities)
 groceries = input("How much do you pay in groceries in a month?\n")
@@ -26,11 +22,19 @@ putilities = Iutil/Iincome
 pgroceries = Igroc/Iincome
 ptranspotation = Itrans/Iincome
 pexpenses = Iexp/Iincome
+
+def percent(type, amount):
+    per = amount/income*100
+
+    return f"Your {type} is {per}% income."
+
 print("Your income is: $", income)
 print("Your expenses are: $", expenses)
 print("Your savings are: $", savings)
 print("Your total left to spend is: $", total)
-print("Your rent payment is: ", prent, "percent of your expenses")
-print("Your utilities payment is: ", putilities, " percent of your expenses")
-print("Your groceries payment is: ", pgroceries, " percent of your expenses")
-print("Your transportation payment is: ", ptranspotation, " percent of your expenses")
+print(percent("rent",rent))
+print(percent("utilities",utilities))
+print(percent("groceries",groceries))
+print(percent("transportation",transportation))
+print(percent("savings",savings))
+print(percent("expenses",expenses))
