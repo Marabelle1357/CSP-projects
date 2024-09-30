8df9d8fc13fcfe3eed6e286bd54d8a327762cb68
Marabelle1357 <mara.anderson@ucas-edu.net>
2024-09-30 00:42:56 +0000

    finished coding project

--- functions/update_budget.py
+++ functions/update_budget.py
@@ -1,27 +1,20 @@
 def inp(type):
-    var = float(input("What is your{type}?"))
+    var = float(input("What is your "+ type + "?"))
     return var
 
 
 print("This code will calculate your bugdet for the month.")
-inp("income")
-inp("rent")
-utilities = input("How much do you pay in utilities in a month?\n")
-Iutil = int(utilities)
-groceries = input("How much do you pay in groceries in a month?\n")
-Igroc = int(groceries)
-transportation = input("How much do you pay in transportation in a month?\n")
-Itrans = int(transportation)
-expenses = Irent + Iutil + Igroc + Itrans
+income = inp("income")
+rent = inp("rent")
+utilities = inp("utilities")
+groceries = inp("groceries")
+transportation = inp("transportation")
+expenses = rent+utilities+groceries+transportation
 Iexp = int(expenses)
-savings = Iincome *0.2
+savings = income *0.2
 Fsav = float(savings)
-total = Iincome-Iexp+Fsav
-prent = Irent/Iincome
-putilities = Iutil/Iincome
-pgroceries = Igroc/Iincome
-ptranspotation = Itrans/Iincome
-pexpenses = Iexp/Iincome
+total = income-Iexp+Fsav
+
 
 def percent(type, amount):
     per = amount/income*100
