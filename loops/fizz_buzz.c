5736afff01d845bfbaf3706873b39c3eed0940d7
Marabelle1357 <mara.anderson@ucas-edu.net>
2024-10-10 20:42:01 +0000

    finished coding project

---
+++ loops/fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int i,usr;
+char one[50], two[50], three[50];
+
+void delay(int sec){
+    int milli = 1000*sec;
+
+    clock_t start = clock();
+
+    while(clock()< start+milli);
+}
+
+int main(void){
+    printf("Give me a number: \n");
+    scanf("%d",&usr);
+    printf("Give me a word: \n");
+    scanf("%s",one);
+    printf("Give me another word: \n");
+    scanf("%s",two);
+    strcat(three,one);
+    strcat(three,two);
+    // create a loop that counts to 50
+    for(i=1;i<=usr;i++){
+        //replace numbers divisible by both with "FizzBuzz"
+        if(i%3==0 && i%5==0){
+            printf("%s\n", three);
+        // replace #'s divisible by 3 with "Fizz"
+        }else if(i%3==0){
+            printf("%s\n", one);
+        //replace #'s divisible by 5 wih "Buzz"
+        }else if(i%5==0){
+            printf("%s\n",two);
+        // print other numbers
+        }else{
+            printf("%d\n",i);
+            delay(50);
+        }
+    }
+    return 0;
+}
